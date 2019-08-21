c6ac496f2ea519c38a74456b3041c9e847241500
tmdghks0612 <dreamspidersh@gmail.com>
2019-08-21 16:09:23 +0900

    Error Log and StringConverter for ErrorLogger added.

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include "ErrorLogger.h"
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "DirectXTK.lib")
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR	lpCmdLine,
+	_In_ int	nCmdShow)
+{
+	//ErrorLogger::Log(E_INVALIDARG, "Test Message");
+	return 0;
+}
